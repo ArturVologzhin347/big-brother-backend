a1d6c06d55965e32957f5d815c9c4fcc27f5a1c8
ArturVologzhin347 <artsoftwareadm@gmail.com>
2022-11-14 00:08:17 +0500

    Database configuration

---
+++ src/main/kotlin/com/ithub/bigbrotherbackend/Profiles.kt
@@ -0,0 +1,6 @@
+package com.ithub.bigbrotherbackend
+
+object Profiles {
+    const val DEV = "dev"
+    const val PROD = "prod"
+}
---
+++ src/main/kotlin/com/ithub/bigbrotherbackend/util/DatabaseExt.kt
@@ -0,0 +1,9 @@
+package com.ithub.bigbrotherbackend.util
+
+import org.springframework.core.io.ClassPathResource
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator
+
+fun ConnectionFactoryInitializer.setPopulatorFromResources(vararg path: String) = setDatabasePopulator(
+    ResourceDatabasePopulator(*path.map { ClassPathResource(it) }.toTypedArray())
+)
