0954188b3a747c3712204bf98025b4c839b3b584
ArturVologzhin347 <artsoftwareadm@gmail.com>
2022-11-13 23:54:59 +0500

    Initial commit

---
+++ src/test/kotlin/com/ithub/bigbrotherbackend/BigBrotherBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ithub.bigbrotherbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BigBrotherBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
